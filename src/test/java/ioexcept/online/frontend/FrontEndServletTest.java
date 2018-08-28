ebc8ca2a0748e2cc82042d3f6573db718773c6aa
Steve S Gee Jr <steve.gee@sprint.com>
2018-08-28 07:10:13 -0500

    added test cases (stubs) and updated pom for junit-4

---
+++ src/test/java/ioexcept/online/frontend/FrontEndServletTest.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package ioexcept.online.frontend;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author lm623642
+ *
+ */
+public class FrontEndServletTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link ioexcept.online.frontend.FrontEndServlet#doPost(HttpServletRequest, HttpServletResponse)}.
+	 */
+	@Test
+	public void testDoPost() {
+//		fail("Not yet implemented");
+		assertEquals(6, 6);
+	}
+
+}
